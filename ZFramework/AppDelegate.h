4b67050946dcf4bc3471b18181b867a7ae7cecab
zhaofeng <zhaofeng@zhaofengtekiMacBook-Pro.local>
2014-01-18 12:39:00 +0800

    Initial Commit

---
+++ ZFramework/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZFramework
+//
+//  Created by zhaofeng on 14-1-18.
+//  Copyright (c) 2014年 GInspire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZFrameworkTests/ZFrameworkTests.m
@@ -0,0 +1,34 @@
+//
+//  ZFrameworkTests.m
+//  ZFrameworkTests
+//
+//  Created by zhaofeng on 14-1-18.
+//  Copyright (c) 2014年 GInspire. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZFrameworkTests : XCTestCase
+
+@end
+
+@implementation ZFrameworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
